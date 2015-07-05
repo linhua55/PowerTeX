0256390abd028928db86bdd96d47cf959ca259b1
Tommy-RMBP <tommy_jiang@foxmail.com>
2015-07-05 21:42:46 +0800

    2015-07-05 习题5.3完成

--- Matlab/Ex5_3.m
+++ Matlab/Ex5_3.m
@@ -29,3 +29,20 @@ Z_m = 1i*X_m; % 互感阻抗
 Z_rm = (Z_r * Z_m)/(Z_r + Z_m); % 转子与互感并联后阻抗
 Z = Z_s + Z_rm; % 异步电机总阻抗
 
+% 定子绕组参数
+N_s = 125; % 定子绕组匝数
+k_N_s = 0.92; % 定子绕组基波系数
+
+% 忽略定子漏阻抗
+E_g_1 = U_N;
+Phi_m_1 = E_g_1/(4.44*f_N*N_s*k_N_s); 
+
+% 考虑定子漏阻抗 空载
+E_g_2 = Z_m/(Z_s+Z_m) * U_N;
+E_g_2_mag = abs(E_g_2);
+Phi_m_2 = E_g_2_mag/(4.44*f_N*N_s*k_N_s); 
+
+% 考虑定子漏阻抗 额定负载
+E_g_3 = (Z_rm)/(Z_s+Z_rm) * U_N;
+E_g_3_mag = abs(E_g_3);
+Phi_m_3 = E_g_3_mag/(4.44*f_N*N_s*k_N_s); 
