bff2229ffe6ab11572c31a38e77353aa1acff16c
Tommy-RMBP <tommy_jiang@foxmail.com>
2015-07-05 19:13:44 +0800

    2015-07-05 习题5.2完成

--- Matlab/Ex5_2.m
+++ Matlab/Ex5_2.m
@@ -1,5 +1,4 @@
-%% 异步电机计算
-% 给定异步电机定转子参数，包括定转子电阻、电感和励磁电感，额定电压，额定转速，额定频率
+% 异步电机降压调速计算
 
 clc; clear;
 
@@ -20,7 +19,7 @@ n_1 = 60*f_N/p; % 同步转速
 
 s_N = (n_1-n_N)/n_1; % 额定转差率
 
-% 定子额定电流
+% 电机阻抗参数
 X_ls = omega_1 * L_ls; % 定子漏抗 
 Z_s = R_s + 1i*X_ls; % 定子阻抗
 X_lr = omega_1 * L_lr; % 转子漏抗
@@ -29,36 +28,31 @@ X_m = omega_1 * L_m;     % 互感电抗
 Z_m = 1i*X_m; % 互感阻抗
 Z_rm = (Z_r * Z_m)/(Z_r + Z_m); % 转子与互感并联后阻抗
 Z = Z_s + Z_rm; % 异步电机总阻抗
-I_sN = U_N/Z; % 定子额定电流
 
-% 转子额定电流
-I_rN = I_sN * Z_rm / Z_r; % 转子额定电流
-
-% 额定励磁电流
-I_mN = I_sN * Z_rm / Z_m; % 额定励磁电流
-
-% 额定电磁功率和额定电磁转矩
-P_eN = 3 * abs(I_rN)^2 * R_r / s_N; % 额定电磁功率
-omega_m = omega_1 / p; % 额定机械角频率
-T_eN = P_eN / omega_m; % 额定电磁转矩
-
-% 理想空载励磁电流 不计损耗 异步机转速为同步速 s=0
-I_m0N = U_N / (Z_r + Z_m); % 理想空载励磁电流
+% U = (1/2)*U_N情况
+U_1_2 = (1/2) * U_N;
+U_2_3 = (2/3) * U_N;
 
 % 临界转差率 临界转矩 (忽略励磁支路)
 s_m = R_r / (sqrt(R_s^2 + omega_1^2*(L_ls+L_lr)^2)); % 临界转差率
-T_em = 3*p*U_N^2 / (2*omega_1*(R_s+sqrt(R_s^2+omega_1^2*(L_ls+L_lr)^2))); % 临界转矩
+T_em_1_2 = 3*p*U_1_2^2 / (2*omega_1*(R_s+sqrt(R_s^2+omega_1^2*(L_ls+L_lr)^2))); % 临界转矩
+T_em_2_3 = 3*p*U_2_3^2 / (2*omega_1*(R_s+sqrt(R_s^2+omega_1^2*(L_ls+L_lr)^2))); % 临界转矩
 
 % 机械特性曲线
 s_tran = 0:0.001:1;
 T_tran = 3*p*U_N^2*R_r.*s_tran ./ (omega_1*((s_tran*R_s+R_r).^2+s_tran.^2*omega_1^2*(L_ls+L_lr)^2));
+T_tran_1_2 = 3*p*U_1_2^2*R_r.*s_tran ./ (omega_1*((s_tran*R_s+R_r).^2+s_tran.^2*omega_1^2*(L_ls+L_lr)^2));
+T_tran_2_3 = 3*p*U_2_3^2*R_r.*s_tran ./ (omega_1*((s_tran*R_s+R_r).^2+s_tran.^2*omega_1^2*(L_ls+L_lr)^2));
+
 hf1 = figure(1);
 set(hf1,'Position',[50 50 400 250]);
 
-h = plot(T_tran,1-s_tran);
+h = plot(T_tran,1-s_tran,T_tran_1_2,1-s_tran,'r',T_tran_2_3,1-s_tran,'g');
 set(gca,'FontSize',12); % 字号设置
 set(gca,'FontName','Times New Roman'); % 字体设置
 set(h,'LineWidth',2); % 线宽设置
 xlabel('电磁转矩{\it T_e}(N·m)','FontSize',12,'FontName','Times New Roman');
 set(gca,'YLim',[0,1.05]);
 ylabel('转速{\it n}(p.u.)','FontSize',12,'FontName','Times New Roman');
+l = legend('$U=U_N$','$U=\frac{1}{2}U_N$','$\it U=\frac{2}{3}U_N$'); % 图例设置
+set(l,'Interpreter','latex','Location','Southeast'); % 图例位置+LaTeX
